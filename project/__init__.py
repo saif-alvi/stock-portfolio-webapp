443ac33d4a86f88e05635572f372007e1413ff46
Saif Alvi <saif-alvi@hotmail.com>
2025-02-10 03:15:39 -0500

    Make functions for all Flask app steps and move it into project __init__.py

---
+++ project/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+from flask.logging import default_handler
+import os
+
+def create_app():
+    # Create the Flask application
+    app = Flask(__name__)
+
+    # Configure the Flask application
+    config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+    app.config.from_object(config_type)
+
+
+    register_blueprints(app)
+    configure_logging(app)
+    return app
+
+def register_blueprints(app):
+    # Import the blueprints
+    from project.stocks import stocks_blueprint
+    from project.users import users_blueprint
+
+    # Since the application instance is now created, register each Blueprint
+    # with the Flask application instance (app)
+
+    app.register_blueprint(stocks_blueprint)
+    app.register_blueprint(users_blueprint, url_prefix='/users')
+
+def configure_logging(app):
+    # Remove the default logger configured by Flask
+    app.logger.removeHandler(default_handler)
+
+    # Logging Configuration
+    file_handler = RotatingFileHandler('instance/flask-stock-portfolio.log', maxBytes=16384, backupCount=20)
+    file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Starting the Flask Stock Portfolio App...')
