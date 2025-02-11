dbbaae658a2be9661663c9a8a150617b65b4cc35
Saif Alvi <saif-alvi@hotmail.com>
2025-02-11 16:48:32 -0500

    added pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import os
+import pytest
+from project import create_app
+from flask import current_app  
+
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app()
+    flask_app.config.from_object('config.TestingConfig')
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context before accessing the logger
+        with flask_app.app_context():
+            current_app.logger.info('In the test_client() fixture...')
+
+        yield testing_client  # this is where the testing happens!
