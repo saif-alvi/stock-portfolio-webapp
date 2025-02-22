637d3fde0b8d466fc1082e3e91d07abd5310381e
saif-alvi <56989215+saif-alvi@users.noreply.github.com>
2025-02-22 00:27:10 -0500

    Revert "Add user registration"

--- project/users/routes.py
+++ project/users/routes.py
@@ -1,9 +1,5 @@
 from . import users_blueprint
-from flask import render_template, flash, abort, request, current_app, redirect, url_for
-from .forms import RegistrationForm
-from project.models import User
-from project import database
-from sqlalchemy.exc import IntegrityError
+from flask import render_template, flash, abort
 
 @users_blueprint.route('/about')
 def about():
@@ -19,24 +15,3 @@ def page_forbidden(e):
 @users_blueprint.route('/admin')
 def admin():
     abort(403)
-
-@users_blueprint.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            try:
-                new_user = User(form.email.data, form.password.data)
-                database.session.add(new_user)
-                database.session.commit()
-                flash(f'Thanks for registering, {new_user.email}!')
-                current_app.logger.info(f'Registered new user: {form.email.data}!')
-                return redirect(url_for('stocks.index'))
-            except IntegrityError:
-                database.session.rollback()
-                flash(f'ERROR! Email ({form.email.data}) already exists.', 'error')
-        else:
-            flash(f"Error in form data!")
-
-    return render_template('users/register.html', form=form)
